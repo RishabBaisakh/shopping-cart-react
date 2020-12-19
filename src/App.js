75bae804b2ccae3dc6da56d9d89150861816e35d
RishabBaisakh <rishab.baisakh@gmail.com>
2020-12-19 00:40:07 -0500

    completed header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <Header />
+      <div className="app__header">
+        <Header />
+      </div>
+
+      <div className="app__body"></div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Header.css";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <h3>Food It Be</h3>
+        <FastfoodIcon />
+      </div>
+
+      <div className="header__right">
+        <div className="header__rightCartItems">
+          <p>1</p>{" "}
+        </div>
+        <ShoppingCartIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
